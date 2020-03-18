fb20381875f682b56466b38d811beb33585da123
seungjae6874 <qkrtmdwo1@gmail.com>
2020-03-18 14:34:36 +0900

    3 17 3주차 회의 및 진행 과 목표

--- Facial-Reco3/face_recog_realtime.py
+++ Facial-Reco3/face_recog_realtime.py
@@ -26,7 +26,7 @@ while True:
         id_,conf = recognizer.predict(roi_gray)#얼굴 유사도 확인 conf는 신뢰도
         print(id_,labels[id_],conf)
         
-        if conf >=70:
+        if conf >=65:
             name = labels[id_] #이름리스트에서 이름 가져오기
             cv2.putText(img,name,(x,y),font,1,(0,0,255),2)
             cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
